5486c1db3202a4825ac259642e74a0eccbe0eb39
Miled Kalbourji <120202170+MiledKalbourji@users.noreply.github.com>
2024-10-12 08:24:49 -0400

    Update Simple-Calucator.py

--- Simple-Calucator.py
+++ Simple-Calucator.py
@@ -1,52 +1,51 @@
 def calculator(): 
-    while True: 
+    while True:
+        # Print options for the user 
+        print("Enter '+' to add two numbers") 
+        print("Enter '-' to subtract two numbers")
+        print("Enter '*' to multiply two numbers")
+        print("Enter '/' to divide two numbers")
+        print("Enter 'quit' to exit")
 
-    # Print option for the user 
-        print ("enter '+' to add two numubers") 
-    print ("enter '-' to add two numubers")
-    print ("enter '*' to add two numubers")
-    print ("enter '/' to add two numubers")
-    print ("enter 'quit' to add two numubers")
+        # Get user input
+        user_input = input(": ")
 
-    #Get user inpout 
-    user_input = input (":")
+        # Check if the user wants to quit
+        if user_input == "quit":
+            break
 
-    #Check if the user wants to quit 
-    if user_input == "quit":
-        breakpoint
-            
-    #check if the user input is a valid oprator 
-    elif user_input in ["+","-","#","/"]:
-        # Get the first number
-        num1 = float(input("Enter a number: "))
+        # Check if the user input is a valid operator
+        elif user_input in ["+", "-", "*", "/"]:
+            # Get the first number
+            num1 = float(input("Enter a number: "))
 
-        # Get second number 
-        num2 = float(input("Enter another number "))
+            # Get the second number
+            num2 = float(input("Enter another number: "))
 
-        #perform the operation based on the user input 
-        if user_input == "+": 
-            result = num1 + num2
-        print (num1, "+", num2, "=", result)
+            # Perform the operation based on the user input
+            if user_input == "+":
+                result = num1 + num2
+                print(f"{num1} + {num2} = {result}")
 
-        #if the user has the input of subtraction 
-    elif user_input == "-": 
-        result = num1 - num2
-        print (num1, "-", num2, "=", result)
+            elif user_input == "-":
+                result = num1 - num2
+                print(f"{num1} - {num2} = {result}")
 
-        #if the user has the input of multiplication 
-    elif user_input == "*": 
-        result = num1 * num2
-        print (num1, "*", num2, "=", result)
+            elif user_input == "*":
+                result = num1 * num2
+                print(f"{num1} * {num2} = {result}")
 
-        #if the user has the input of divison
-    elif user_input == "/": 
-        result = num1 / num2
-        print (num1, "/", num2, "=", result)
-
-    else: 
-        # In case of invalid input 
-        print("Invaild input")
+            elif user_input == "/":
+                # Handle division by zero
+                if num2 == 0:
+                    print("Error: Division by zero is not allowed.")
+                else:
+                    result = num1 / num2
+                    print(f"{num1} / {num2} = {result}")
+        else:
+            # In case of invalid input
+            print("Invalid input, please try again.")
 
 
-#Call the calculator function to start the program 
+# Call the calculator function to start the program 
 calculator()
